087e8c3eff9d34666ea82b8a218c1d4e3c907b3e
siddhikharde <siddhikharde2005@gmail.com>
2025-12-25 10:34:45 +0530

    health rout implemented

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 app.use(express.json());
 const PORT=8000;
 
+
+app.get('/health',(req,res)=>{
+    res.json({
+        success:true,
+        message:"Server is healthy",
+    })
+})
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
